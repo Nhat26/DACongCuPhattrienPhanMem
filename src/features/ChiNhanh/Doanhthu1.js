adaa19854e4a3ae6527826043b6003dc7e1c1464
Vo Huy Hoang <huyhoangvo2k1@gmail.com>
2023-03-22 10:46:58 +0700

    edit DoanhThu1

--- src/features/ChiNhanh/Doanhthu1.js
+++ src/features/ChiNhanh/Doanhthu1.js
@@ -1,4 +1,4 @@
-import React,  { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Modal } from 'react-bootstrap'
 import axios from "axios";
 import styles from "./TungChiNhanh.module.css"
@@ -12,15 +12,15 @@ const Doanhthu1 = () => {
 
 
     const navigate = useNavigate();
- 
 
 
 
-    const [Manghoadon, setManghoadon]= useState([]);
+
+    const [Manghoadon, setManghoadon] = useState([]);
 
 
 
-    const [Mangdoanhthu, setMangdoanhthu]= useState([]);
+    const [Mangdoanhthu, setMangdoanhthu] = useState([]);
     let SLCHINHANH = localStorage.getItem('SLCHINHANH');
     console.log('trang doan thu hin sl chi nhan', SLCHINHANH);
 
@@ -29,75 +29,75 @@ const Doanhthu1 = () => {
 
         const url = `http://localhost:5001/HoaDon`
         axios.get(url)
-                  .then(response => {
-                      const result = response.data;
-                      const { status, message, data } = result;
-                      if (status !== 'SUCCESS') {
-                          alert(message, status)
-                      }
-                      else {
-                        setManghoadon(data)
-    
-                        
-                          console.log("ben trang doanthu", data)
-                      }
-                  })
-                  .catch(err => {
-                      console.log(err)
-                  })
-
-                 
+            .then(response => {
+                const result = response.data;
+                const { status, message, data } = result;
+                if (status !== 'SUCCESS') {
+                    alert(message, status)
                 }
-   let j=[1,2,3];
-   console.log("trang doan thu nhat", j.length)
-    let soluonghoadon=0;
-    let Tongthunhap=0;
-    
+                else {
+                    setManghoadon(data)
+
+
+                    console.log("ben trang doanthu", data)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+
+    }
+    let j = [1, 2, 3];
+    console.log("trang doan thu nhat", j.length)
+    let soluonghoadon = 0;
+    let Tongthunhap = 0;
+
     console.log("trang doan thu nhat 1", Manghoadon.length)
-    const Hamhienthidoanthu= ()=>{
+    const Hamhienthidoanthu = () => {
         console.log("da chay len dc");
 
         // let itemm={
         //     _id: Data[i]._id,
         //     ChiNhanh: Data[i].ChiNhanh,
         //         SoLuongHD: soluonghoadon,
-  
+
         // }
-        
-        for(let i=1; i<= SLCHINHANH; i++){
-            soluonghoadon=0;
-   Tongthunhap=0;
-           
-            for(let j=0; j< Manghoadon.length ; j ++){
+
+        for (let i = 1; i <= SLCHINHANH; i++) {
+            soluonghoadon = 0;
+            Tongthunhap = 0;
+
+            for (let j = 0; j < Manghoadon.length; j++) {
                 console.log("vong for in chinhanh doanhthu", Manghoadon[j].ChiNhanh);
-                if(Manghoadon[j].ChiNhanh==i){
-                    soluonghoadon=soluonghoadon+1;
+                if (Manghoadon[j].ChiNhanh == i) {
+                    soluonghoadon = soluonghoadon + 1;
                     Tongthunhap = Tongthunhap + Manghoadon[j].Gia;
 
                 }
             }
 
-        //     let itemm={
-        //     // _id: Data[i]._id,
-        //     ChiNhanh: Data[i].ChiNhanh,
-        //         SoLuongHD: soluonghoadon,
-  
-        // }
+            //     let itemm={
+            //     // _id: Data[i]._id,
+            //     ChiNhanh: Data[i].ChiNhanh,
+            //         SoLuongHD: soluonghoadon,
 
-                console.log("chinhanh doanhthu", Manghoadon);
-                console.log("tongthunhan", soluonghoadon);
-                console.log("tongthunhan", Tongthunhap);
-                
-                
-          
+            // }
+
+            console.log("chinhanh doanhthu", Manghoadon);
+            console.log("tongthunhan", soluonghoadon);
+            console.log("tongthunhan", Tongthunhap);
+
+
+
 
 
 
         }
 
     }
-    const handlePostShow=() => {
-         Getallhoadon();
+    const handlePostShow = () => {
+        Getallhoadon();
         Hamhienthidoanthu();
     }
 
@@ -106,44 +106,44 @@ const Doanhthu1 = () => {
 
 
 
-    
+
 
     useEffect(() => {
-            // Getnhanvien();
-            // Getallhoadon();
-            // hamgopmang();
-            // Getallchitiethoadon();
-        }, [])
-  
+        // Getnhanvien();
+        // Getallhoadon();
+        // hamgopmang();
+        // Getallchitiethoadon();
+    }, [])
 
-  
-  var Xep=true;
-  return (
-    <div>
-      <div>sdfgsfsdhukf</div>
-  
 
-      <div>
-            <div className='row'>
-                <div className='mt-5 mb-4'>
-                    <Button variant='primary' onClick={() => { handlePostShow() }}><i className='fa fa-plu'></i>
-                        Thêm Nhân Viên
-                    </Button>
+
+    var Xep = true;
+    return (
+        <div>
+            <div>sdfgsfsdhukf</div>
+
+
+            <div>
+                <div className='row'>
+                    <div className='mt-5 mb-4'>
+                        <Button variant='primary' onClick={() => { handlePostShow() }}><i className='fa fa-plu'></i>
+                            Thêm Nhân Viên
+                        </Button>
+                    </div>
                 </div>
-            </div>
-            <div className='row'>
-                <div className='table-responsive'>
-                    <table className='table table-striped table-hover table-bordered'>
-                        <thead>
-                            <tr>
-                            <th>Chi Nhánh</th>
-                                <th>Tổng SL Hoá Đơn</th>
-                                <th>Tổng Thu Nhập</th>
-                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {/* {Manggop.map((item) =>
+                <div className='row'>
+                    <div className='table-responsive'>
+                        <table className='table table-striped table-hover table-bordered'>
+                            <thead>
+                                <tr>
+                                    <th>Chi Nhánh</th>
+                                    <th>Tổng SL Hoá Đơn</th>
+                                    <th>Tổng Thu Nhập</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {/* {Manggop.map((item) =>
                                 <tr key={item._id}>
                                     <td>{item.hoten}</td>
                                     <td>{item.sdt}</td>
@@ -157,21 +157,21 @@ const Doanhthu1 = () => {
                                     </td>
                                 </tr>
                             )} */}
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
 
 
 
-                    
+
+                    </div>
                 </div>
             </div>
-            </div>
-        
-        
-      
-      {/* <a>Code Chuc nang</a> */}
-    </div>
-  );
+
+
+
+            {/* <a>Code Chuc nang</a> */}
+        </div>
+    );
 }
 
 export default Doanhthu1;
